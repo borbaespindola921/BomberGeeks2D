45cfef02a19d5c9cf01248701503ae38ff542a5e
borbaespindola921 <jose.espindola@alunoeleva.com.br>
2023-03-04 10:07:22 -0300

    Player Network

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    Rigidbody2D rb;
+    float inputX;
+    float inputY;
+    public float speed = 3;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(isLocalPlayer)
+        {
+            inputX = Input.GetAxisRaw("Horizontal");
+            inputY = Input.GetAxisRaw("Vertical");
+
+            rb.velocity = new Vector2(inputX, inputY) * speed;
+        }       
+  
+    }   
+
+}
